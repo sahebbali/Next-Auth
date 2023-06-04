bed4d8ebda24e00f72207a41d75646c6b78117bd
sahebbali <sahebbali253@gmail.comm>
2023-06-04 22:12:19 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Providers from '@/components/Providers'
+import AppBar from '@/components/AppBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <body>
+      <Providers>
+        <AppBar />
+        {children}
+      </Providers>
+    </body>
+  </html>
   )
 }
---
+++ app/userpost/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h3> Hello this is Post Page!</h3>
+    </div>
+  )
+}
+
+export default page
